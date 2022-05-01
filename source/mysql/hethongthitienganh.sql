ff2ff3918a64d7f2e5174786eb685755ec7bec11
TanIsLearning <90591448+TanIsLearning@users.noreply.github.com>
2022-05-01 16:34:15 +0700

    Add files via upload

---
+++ source/mysql/hethongthitienganh.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 01, 2022 at 09:33 AM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hethongthitienganh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cauhoi`
+--
+
+DROP TABLE IF EXISTS `cauhoi`;
+CREATE TABLE IF NOT EXISTS `cauhoi` (
+  `MaCauHoi` int NOT NULL AUTO_INCREMENT,
+  `CauHoi` text NOT NULL,
+  `LoaiCauHoi` text NOT NULL,
+  `A` text NOT NULL,
+  `B` text NOT NULL,
+  `C` text,
+  `D` text,
+  `DapAnDung` text NOT NULL,
+  KEY `id` (`MaCauHoi`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `cauhoi`
+--
+
+INSERT INTO `cauhoi` (`MaCauHoi`, `CauHoi`, `LoaiCauHoi`, `A`, `B`, `C`, `D`, `DapAnDung`) VALUES
+(1, 'We\'d better get ___________ to check the wiring before we start decorating.', 'Từ vựng', 'an electrician', 'a mechanic', 'a consumer', 'an engineer', 'A'),
+(2, 'what would you ________ for dinner?', 'Loại từ', 'like', 'likes', 'to like', 'liking', 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoidung`
+--
+
+DROP TABLE IF EXISTS `nguoidung`;
+CREATE TABLE IF NOT EXISTS `nguoidung` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `TenDangNhap` varchar(50) NOT NULL,
+  `MatKhau` varchar(50) NOT NULL,
+  KEY `id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
