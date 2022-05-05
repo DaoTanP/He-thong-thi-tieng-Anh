d2efb9f1229e0814a7d3efc64e48b01a9c349240
Phat <daotanp@gmail.com>
2022-05-05 12:27:13 +0700

    a

--- script.js
+++ script.js
@@ -20,14 +20,9 @@ function getCookie(cname) {
     return "";
 }
 
-function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 365);
-        }
-    }
+function checkCookie(cname) {
+    let c = getCookie(cname);
+    if (c != "")
+        return true;
+    return false;
 }
