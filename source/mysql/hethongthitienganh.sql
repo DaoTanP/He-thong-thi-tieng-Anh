575d09d2051b79b64b86675c5520192bf1334e3d
DVinh <sadasf@gmail.com>
2022-05-06 10:40:34 +0700

    :)))

--- source/mysql/hethongthitienganh.sql
+++ source/mysql/hethongthitienganh.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: May 01, 2022 at 09:33 AM
--- Server version: 8.0.27
--- PHP Version: 7.4.26
+-- Host: 127.0.0.1
+-- Generation Time: May 06, 2022 at 05:36 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `hethongthitienganh`
+-- Database: `thita`
 --
 
 -- --------------------------------------------------------
@@ -27,18 +28,16 @@ SET time_zone = "+00:00";
 -- Table structure for table `cauhoi`
 --
 
-DROP TABLE IF EXISTS `cauhoi`;
-CREATE TABLE IF NOT EXISTS `cauhoi` (
-  `MaCauHoi` int NOT NULL AUTO_INCREMENT,
-  `CauHoi` text NOT NULL,
-  `LoaiCauHoi` text NOT NULL,
-  `A` text NOT NULL,
-  `B` text NOT NULL,
-  `C` text,
-  `D` text,
-  `DapAnDung` text NOT NULL,
-  KEY `id` (`MaCauHoi`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `cauhoi` (
+  `MaCauHoi` int(11) NOT NULL,
+  `CauHoi` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `LoaiCauHoi` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `A` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `B` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `C` text COLLATE utf8mb4_unicode_ci,
+  `D` text COLLATE utf8mb4_unicode_ci,
+  `DapAnDung` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `cauhoi`
@@ -46,7 +45,9 @@ CREATE TABLE IF NOT EXISTS `cauhoi` (
 
 INSERT INTO `cauhoi` (`MaCauHoi`, `CauHoi`, `LoaiCauHoi`, `A`, `B`, `C`, `D`, `DapAnDung`) VALUES
 (1, 'We\'d better get ___________ to check the wiring before we start decorating.', 'Từ vựng', 'an electrician', 'a mechanic', 'a consumer', 'an engineer', 'A'),
-(2, 'what would you ________ for dinner?', 'Loại từ', 'like', 'likes', 'to like', 'liking', 'A');
+(2, 'what would you ________ for dinner?', 'Loại từ', 'like', 'likes', 'to like', 'liking', 'A'),
+(3, 'Choose a word that has different stress pattern', 'Trọng âm', 'athletics', 'hopelessness', 'resident', 'tolerance', 'A'),
+(4, 'Choose a word that has different stress pattern:', 'Trọng âm', 'develop', 'overall', 'commitment', 'investment', 'B');
 
 -- --------------------------------------------------------
 
@@ -54,13 +55,43 @@ INSERT INTO `cauhoi` (`MaCauHoi`, `CauHoi`, `LoaiCauHoi`, `A`, `B`, `C`, `D`, `D
 -- Table structure for table `nguoidung`
 --
 
-DROP TABLE IF EXISTS `nguoidung`;
-CREATE TABLE IF NOT EXISTS `nguoidung` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `TenDangNhap` varchar(50) NOT NULL,
-  `MatKhau` varchar(50) NOT NULL,
-  KEY `id` (`id`)
+CREATE TABLE `nguoidung` (
+  `id` int(11) NOT NULL,
+  `TenDangNhap` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `MatKhau` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cauhoi`
+--
+ALTER TABLE `cauhoi`
+  ADD KEY `id` (`MaCauHoi`);
+
+--
+-- Indexes for table `nguoidung`
+--
+ALTER TABLE `nguoidung`
+  ADD KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cauhoi`
+--
+ALTER TABLE `cauhoi`
+  MODIFY `MaCauHoi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `nguoidung`
+--
+ALTER TABLE `nguoidung`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
